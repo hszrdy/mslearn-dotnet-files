f12b91ab57ff672465a91098788e3f34019a3102
hrkrdy <63145494+hszrdy@users.noreply.github.com>
2026-02-21 16:21:47 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var currentDirectory = Directory.GetCurrentDirectory();
+        var storesDirectory = Path.Combine(currentDirectory, "stores");
+        var salesFiles = FindFiles(storesDirectory);
+
+        foreach (var file in salesFiles)
+        {
+            System.Console.WriteLine(file);
+        }
+    }
+
+    static IEnumerable<string> FindFiles(string folderName)
+    {
+        List<string> salesFiles = new List<string>();
+        var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+        foreach (var file in foundFiles)
+        {
+            var extention = Path.GetExtension(file);
+            if (extention == ".json")
+            {
+                salesFiles.Add(file);
+            }
+        }
+        return salesFiles;
+    }
+}
